92fc286491f7619110d9c0de9d23dfd14d032576
Marwan Kishk <kishkmarwan49@gmail.com>
2025-06-16 23:10:25 +0300

    debug

--- app/main.py
+++ app/main.py
@@ -4,5 +4,8 @@ from app.routes.ideas import router as idea_router
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"message": "Hello from FastAPI on Vercel"}
 app.include_router(auth_router)
 app.include_router(idea_router)
